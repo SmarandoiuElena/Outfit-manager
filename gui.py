d5e4807486b618529b1b1f29eeab30f229aaaaf0
Elena Smarandoiu <smarandoiuelenaandrada@gmail.com>
2026-05-02 15:13:49 +0300

    Added the menu with the neccesary buttons for the app.
    Signed-off-by: Elena Smarandoiu <smarandoiuelenaandrada@gmail.com>

--- gui.py
+++ gui.py
@@ -16,8 +16,57 @@ class Outfit_manager:
         canvas = Canvas(self.window, height=200, width=200, bg = "white")
         canvas.grid(row=0, column=1)
    
-        label = Label(self.window, text="heio")
-        label.grid(row=1, column=0)
+        # creating the menu
+        menubar = Menu(self.window)
+        
+        # adding the main menu
+        main_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label = "Main menu", menu = main_menu)
+        main_menu.add_command(label = "Refresh", command = None)
+        main_menu.add_command(label = "Exit", command = self.window.destroy)
+        
+        # adding the 'add item' menu
+        add_item = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label = "Add item", menu = add_item)
+        add_item.add_command(label = "Add top", command = None)
+        add_item.add_command(label = "Add bottom", command = None)
+        add_item.add_command(label = "Add shoes", command = None)
+        add_item.add_command(label = "Add dresses", command = None)
+        add_item.add_separator()
+        add_item.add_command(label = "Add accesories", command = None)
+        add_item.add_command(label = "Add bag", command = None)
+        add_item.add_command(label = "Add another item", command = None)
+        
+        # adding the 'View wardrobe' menu
+        view_wardrobe = Menu(menubar, tearoff = 0)
+        menubar.add_cascade(label = "View wardrobe", menu = view_wardrobe)
+        view_wardrobe.add_command(label = "View all", command = None)
+        view_wardrobe.add_separator()
+        view_wardrobe.add_command(label = "View tops", command = None)
+        view_wardrobe.add_command(label = "View bottoms", command = None)
+        view_wardrobe.add_command(label = "View dresses", command = None)
+        view_wardrobe.add_command(label = "View shoes", command = None)
+        view_wardrobe.add_separator()
+        view_wardrobe.add_command(label = "View accesories", command = None)
+        view_wardrobe.add_command(label = "View bags", command = None)
+        view_wardrobe.add_command(label = "View others", command = None)
+        
+        # adding the 'Create outfit' menu
+        create_outfit = Menu(menubar, tearoff = 0)
+        menubar.add_cascade(label = "Create outfit", menu = create_outfit)
+        create_outfit.add_command(label = "Randomize", command = None)
+        create_outfit.add_command(label = "Create from scratch", command = None)
+        
+        # adding the 'View outfits' menu
+        view_outfis = Menu(menubar, tearoff = 0)
+        menubar.add_cascade(label = "View outfits", menu = view_outfis)
+
+        # adding the 'About us' menu
+        about_us = Menu(menubar, tearoff = 0)
+        menubar.add_cascade(label = "About us", menu = about_us)
+        
+        # this adds the menu
+        self.window.config(menu = menubar)
         
 window = Tk()
 wardrobe = []
