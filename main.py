1f9f2d39a761851488b8f6776bc8e1b9201113e0
Elena Smarandoiu <smarandoiuelenaandrada@gmail.com>
2026-05-05 16:23:58 +0300

    Bia moddified main.py to include all the imports necessary: Wardrobe, csv_handler.
    Signed-off-by: Elena Smarandoiu <smarandoiuelenaandrada@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from gui import Outfit_manager
-from music import start_music
 from tkinter import Tk
 
+from gui import Outfit_manager
+from music import start_music
+from models import Wardrobe
+from csv_handler import load_items, load_outfits
+
+
 window = Tk()
-wardrobe = []
+
+wardrobe = Wardrobe()
+wardrobe.items = load_items()
+wardrobe.outfits = load_outfits(wardrobe.items)
+
 start_music("music/Music1.mpeg")
+
 app = Outfit_manager(window, wardrobe)
+
 window.mainloop()
